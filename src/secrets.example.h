80650aeb6afd19ac6968181a678c4ae9793b98a3
Vladimir Milkov <milkov@joom.com>
2025-01-26 15:55:35 +0300

    Init

---
+++ src/secrets.example.h
@@ -0,0 +1,13 @@
+/************************* WiFi config ****************************************/
+
+#define WLAN_SSID ""
+
+#define WLAN_PASS ""
+
+/************************* MQTT server config *********************************/
+
+#define MQTT_SERVER ""
+
+/************************* Board specific setup *******************************/
+
+#define MQTT_TOPIC_SET_VALUE_1 ""
